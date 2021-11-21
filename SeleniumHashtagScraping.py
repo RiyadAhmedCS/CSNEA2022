5a50b5276f4e8e9d32c823658ec6235481793e18
RiyadAhmedCS <riyadahmedcsnea@gmail.com>
2021-11-21 19:11:32 +0000

    Started Code for scraping instagram hashtags

---
+++ SeleniumHashtagScraping.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import time
+
+USERNAME = "riyadahmedcsnea@gmail.com"
+PASSWORD = "uGPZ6agPF5zTu3"
+
+driverPath = "C:\\Users\\riyad\\Documents\\GitHub\\CSNEA2022\\chromedriver.exe"
+driver = webdriver.Chrome(driverPath)
+driver.get("http://www.instagram.com")
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+cookiesaccept = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Accept All")]'))).click()
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+username.clear()
+username.send_keys(USERNAME)
+password.clear()
+password.send_keys(PASSWORD)
+Login_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+
+time.sleep(3)
+notnow = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+
+#<button class="aOOlW  bIiDR  " tabindex="0">Accept All</button>
